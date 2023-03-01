76a0b8ff97aea7bbab707b09b6803f53b5ccf1cf
Arnish <arnishmishra03@gmail.com>
2023-03-02 01:52:36 +0530

    made navigation bar responsive

--- portfolio.js
+++ portfolio.js
@@ -1,1 +1,8 @@
-but
+function opennav(){
+    document.getElementById("sidebar").style.width="16%";
+    document.getElementById("main").style.marginLeft="22%";
+}
+function closenav(){
+    document.getElementById("sidebar").style.width="0";
+    document.getElementById("main").style.marginLeft="0";
+}
