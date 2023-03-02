e0dfa56d955fb4fce2d9c35c52dbe29b0dbf2831
Arnish <arnishmishra03@gmail.com>
2023-03-02 11:00:53 +0530

    made nav bar work for all the links

--- portfolio.js
+++ portfolio.js
@@ -1,8 +1,10 @@
 function opennav(){
     document.getElementById("sidebar").style.width="16%";
-    document.getElementById("main").style.marginLeft="22%";
+    document.getElementById("main").style.marginLeft="16%";
+    document.getElementById("menu").style.backgroundColor=("#659dbd");
 }
 function closenav(){
     document.getElementById("sidebar").style.width="0";
     document.getElementById("main").style.marginLeft="0";
+    document.getElementById("menu").style.backgroundColor=("white");
 }
