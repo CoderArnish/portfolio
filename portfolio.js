6c0a4091fd58095b5d1e121387499bee0f967431
Arnish <arnishmishra03@gmail.com>
2023-03-02 11:50:16 +0530

    working on toggle menu button

--- portfolio.js
+++ portfolio.js
@@ -2,9 +2,19 @@ function opennav(){
     document.getElementById("sidebar").style.width="16%";
     document.getElementById("main").style.marginLeft="16%";
     document.getElementById("menu").style.backgroundColor=("#659dbd");
+    document.getElementById("menu");
 }
 function closenav(){
     document.getElementById("sidebar").style.width="0";
     document.getElementById("main").style.marginLeft="0";
     document.getElementById("menu").style.backgroundColor=("white");
+}
+function nav(){
+    var x = document.getElementById("sidebar").style.width;
+    if(x==0){
+        opennav();
+    }
+    else{
+        closenav();
+    }
 }
