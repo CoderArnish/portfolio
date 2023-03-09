4fe517b3399ae19c5fe59e74a0f757a1f0fca8fb
Arnish <arnishmishra03@gmail.com>
2023-03-10 00:00:25 +0530

    final touches and makes sure everything works

--- portfolio.js
+++ portfolio.js
@@ -1,6 +1,6 @@
 function opennav(){
-    document.getElementById("sidebar").style.width="16%";
-    document.getElementById("main").style.marginLeft="16%";
+    document.getElementById("sidebar").style.width="200px";
+    document.getElementById("main").style.marginLeft="200px";
     document.getElementById("menu").style.backgroundColor=("#659dbd");
     document.getElementById("menu");
 }
@@ -9,7 +9,7 @@ function closenav(){
     document.getElementById("main").style.marginLeft="0";
     document.getElementById("menu").style.backgroundColor=("white");
 }
-function nav(){
+/*function nav(){
     var x = document.getElementById("sidebar");
     if(x.style.width==0){
         opennav();
@@ -17,4 +17,4 @@ function nav(){
     else{
         closenav();
     }
-}
+}*/
